41ea171e4d6146cf7a04834cf41c0caea1f24a33
Jason Boothman <jboothman@huntington.edu>
2021-07-26 08:12:53 -0400

    Updated for EE6. Remove individual member group features. Hide ‘Files’, ‘Developer’ menu, and ‘preview’ button for non-super admins.

--- system/user/addons/control_panel_tweaks/addon.setup.php
+++ system/user/addons/control_panel_tweaks/addon.setup.php
@@ -5,7 +5,7 @@ return array(
       'docs_url'	      => 'https://github.com/oldmanboothman/Control-Panel-Tweaks',
       'name'            => 'Control Panel Tweaks',
       'description'     => 'This add-on modifies some of the default control panel styling as well as gives you options to make various overrides such as hiding certain parts of the navigation.',
-      'version'         => '1.0.0',
+      'version'         => '2.0.0',
       'namespace'       => 'OldManBoothman\ControlPanelTweaks',
-      'settings_exist'  => TRUE
+      'settings_exist'  => FALSE
 );
--- system/user/addons/control_panel_tweaks/ext.control_panel_tweaks.php
+++ system/user/addons/control_panel_tweaks/ext.control_panel_tweaks.php
@@ -7,26 +7,24 @@
  * @author		Jason Boothman
  * @copyright	Copyright (c) 2017, Reusser Design
  * @link		https://github.com/oldmanboothman
- * @since		1.0.0
+ * @since		2.0.0
  * @filesource 	./system/user/addons/control_panel_tweaks/ext.control_panel_tweaks.php
  */
 class Control_panel_tweaks_ext {
-
-	var $settings = array();
 
 	/**
 	 * Constructor
 	 *
 	 */
-	public function __construct($settings = '')
+	public function __construct()
 	{
-		$this->settings = $settings;
+		//$this->settings = $settings;
 
 		// required extension properties
 		$this->name				= 'Control Panel Tweaks';
-		$this->version			= '1.0.0';
-		$this->description		= 'This add-on modifies some of the default control panel styling as well as gives you options to make various overrides such as hiding certain parts of the navigation.';
-		$this->settings_exist	= 'y';
+		$this->version			= '2.0.0';
+		$this->description		= 'This add-on hides certain parts of the navigation.';
+		$this->settings_exist	= 'n';
 
 		ee()->load->library('session');
 	}
@@ -76,28 +74,6 @@ class Control_panel_tweaks_ext {
 		ee()->db->update('extensions', array('version' => $this->version));
 	}
 
-	function settings()
-	{
-		// Get all user groups that have access to the control panel
-		$query = ee()->db->select('group_id, group_title')->get_where('member_groups', array('can_access_cp' => 'y'));
-
-		$groups = array();
-
-		foreach ($query->result() as $row)
-		{
-				$groups[$row->group_id] = $row->group_title;
-		}
-
-		$settings = array();
-
-		// Settings for hiding buttons
-		$settings['hide_files_button'] = array('ms', $groups, '');
-		$settings['hide_developer_button'] = array('ms', $groups, '');
-		$settings['hide_preview_button'] = array('ms', $groups, '');
-
-		return $settings;
-	}
-
 	// ------------------------------------------------------
     //
     /**
@@ -111,47 +87,18 @@ class Control_panel_tweaks_ext {
      */
     public function cp_css_end()
     {
-		//Set Defaults
 		$css = '';
-		$group_id = ee()->session->userdata('group_id');
 
-		//Bring in all our custom CSS files
-		$cp_style = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/cp.css');
-		$wygwam_style = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/wygwam.css');
 		$hide_files_button = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/hide-files-button.css');
 		$hide_developer_button = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/hide-developer-button.css');
 		$hide_preview_button = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/hide-preview-button.css');
-		$upper_nav = file_get_contents( PATH_THIRD . '/control_panel_tweaks/css/upper-nav.css');
 
-		//Hide buttons
-		if (isset($this->settings['hide_files_button'])) {
-			foreach ($this->settings['hide_files_button'] as $row)
-			{
-				if ($row == $group_id) {
-					$css .= $hide_files_button;
-				}
-			}
-		}
-
-		if (isset($this->settings['hide_developer_button'])) {
-			foreach ($this->settings['hide_developer_button'] as $row)
-			{
-				if ($row == $group_id) {
-					$css .= $hide_developer_button;
-				}
-			}
-		}
-
-		if (isset($this->settings['hide_preview_button'])) {
-			foreach ($this->settings['hide_preview_button'] as $row)
-			{
-				if ($row == $group_id) {
-					$css .= $hide_preview_button;
-				}
-			}
-		}
-
-        $css .= $cp_style . $structure_style . $assets_style . $wygwam_style . $upper_nav;
+        //Hide navigation for non-Super Admins
+        if (ee()->session->userdata('group_id') != 1) {
+            $css .= $hide_files_button;
+            $css .= $hide_developer_button;
+            $css .= $hide_preview_button;
+        }
 
         $user_css_location = ee()->config->item('control_panel_tweaks_user_css');
 
